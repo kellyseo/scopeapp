f50cce5ecb5f810106e3964ed296494fc06fa07f
Kelly Seo <yjseo@ucsd.edu>
2018-10-31 03:23:24 -0700

    getting the files

---
+++ js/introHCI.js
@@ -0,0 +1,21 @@
+// Call this function when the page loads (the jQuery "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript is connected");
+	});
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+}
+
+$(".project #myelement").mousemove(changeText);
+function changeText(event) {
+	$(this).text("WTF");
+}
