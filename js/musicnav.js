de77b8aa70862d3f35b0fe28a5cdd1e9834b69bb
yuw446 <43831158+yuw446@users.noreply.github.com>
2018-11-01 22:34:25 -0700

    Fixing the layout issues of Music Page

---
+++ js/musicnav.js
@@ -0,0 +1,60 @@
+// Call this function when the page loads (the jQuery "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript is connected");
+	});
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+}
+
+$(".project #myelement").mousemove(changeText);
+function changeText(event) {
+	$(this).text("WTF");
+}
+
+function myFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+// Codes for the social media section e.g. the tablinks
+
+function openTab(evt, platformName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(platformName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
